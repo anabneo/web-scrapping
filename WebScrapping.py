6c9eb79a67cb9f60f56af8afec99d1967654cc49
“Ana <ana.neo@mercadolivre.com>
2020-06-19 12:58:08 -0300

    Exceptions

--- WebScrapping.py
+++ WebScrapping.py
@@ -3,11 +3,17 @@ from bs4 import BeautifulSoup
 from urllib.error import URLError, HTTPError
 
 url = 'https://www.alura.com.br/'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
 
-req = Request(url, headers = headers)
-response = urlopen(req)
-html = response.read()
+try:
+    req = Request(url, headers=headers)
+    response = urlopen(req)
+    html = response.read()
+    print(html)
 
-print(html)
+except HTTPError as e:
+    print(e.status, e.reason)
 
+except URLError as e:
+    print(e.reason)
