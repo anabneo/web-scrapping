f8e1a1b9b672a07ca1413f4da2a570dbdf31e8a9
“Ana <ana.neo@mercadolivre.com>
2020-06-19 12:52:59 -0300

    fix: forbidden 403

--- WebScrapping.py
+++ WebScrapping.py
@@ -1,12 +1,13 @@
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from urllib.error import URLError, HTTPError
 
-url = 'https://alura-site-scraping.herokuapp.com/hello-world.php'
+url = 'https://www.alura.com.br/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
 
-response = urlopen(url)
+req = Request(url, headers = headers)
+response = urlopen(req)
 html = response.read()
 
-soup = BeautifulSoup(html, 'html.parser')
-
-print(soup.find('h1', {'class': 'sub-header'}).get_text())
+print(html)
 
